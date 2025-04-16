15e1ac81944a53e653208ffb848dfd6ace31af81
Ben Richardson <benrichardson830@gmail.com>
2025-04-16 13:09:32 +0200

    Change context to include more folders

--- isyhand_driver/scripts/run.sh
+++ isyhand_driver/scripts/run.sh
@@ -30,6 +30,8 @@ fi
 echo "🛠  Building Docker image: $IMAGE_NAME"
 docker build \
   --build-arg USERNAME="admin" \
+  --build-arg LOCAL_WS=$ISYHAND_ROS_WS \
+  --build-arg CONTAINER_WS=$CONTAINER_WS
   -t $IMAGE_NAME \
   -f $ISYHAND_ROS_WS/src/isyhand_driver/docker/Dockerfile.base \
   $ISYHAND_ROS_WS/src/isyhand_driver/docker
